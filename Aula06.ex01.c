f836c3aa297e11bd25e53d618bf0749b67b24806
CarvalhoCintia <111870425+CarvalhoCintia@users.noreply.github.com>
2022-08-29 11:23:00 -0300

    Rename Aula06.ex01.cpp to Aula06.ex01.c

--- Aula06.ex01.c
+++ Aula06.ex01.c
@@ -1,5 +1,5 @@
 
-/*1 - Crie dois vetores numÈricos de dimens„o 5 com valores informados pelo usu·rio.
+/*1 - Crie dois vetores num√©ricos de dimens√£o 5 com valores informados pelo usu√°rio.
 Apresente a soma do primeiro vetor com o inverso do segundo.*/
 #include<stdio.h>
 #include<locale.h>
@@ -7,12 +7,12 @@ Apresente a soma do primeiro vetor com o inverso do segundo.*/
 		setlocale(LC_ALL,"");
     int v1[5],v2[5],i;
     for(i=0;i<5;i++){ 
-        printf("Digite os n˙meros do primeiro vetor [%d]:",i);
+        printf("Digite os n√∫meros do primeiro vetor [%d]:",i);
         scanf("%d",&v1[i]);
     }
     printf("\n");
 	for(i=0; i<5; i++){
-		printf("Digitar os n˙mero do segundo vetor [%d]:", i);
+		printf("Digitar os n√∫mero do segundo vetor [%d]:", i);
 		scanf("%d", &v2[i]);
 	}
 	//Processamento
